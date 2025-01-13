1e18aa5e5e861f126ae6681be0000d1a04a2bea2
dipoesan <esan.dipo@gmail.com>
2025-01-12 22:29:15 -0500

    adjustments

--- json.go
+++ json.go
@@ -26,11 +26,12 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		w.WriteHeader(500)
 		return
 	}
-	func WriteJSONResponse(w http.ResponseWriter, code int, dat []byte) {
-		w.WriteHeader(code)
-		if _, err := w.Write(dat); err != nil {
-			log.Printf("failed to write response: %v", err)
-		}
+	WriteJSONResponse(w, code, dat)
+}
+func WriteJSONResponse(w http.ResponseWriter, code int, dat []byte) {
+	w.WriteHeader(code)
+	if _, err := w.Write(dat); err != nil {
+		log.Printf("failed to write response: %v", err)
 	}
 	// w.WriteHeader(code)
 	// w.Write(dat)
